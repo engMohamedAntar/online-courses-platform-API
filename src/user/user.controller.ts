31525cce81306aa46b2589377eb34ca122b02a55
MohamedAntar <aboantar852003@gmail.com>
2025-09-12 19:04:26 +0300

    made UserController public

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -15,9 +15,10 @@ import { CreateUserDto } from './dto/createUser.dto';
 import { Roles } from '../common/decorators/roles.decorator';
 import { RolesGuard } from '../common/guards/roles.guards';
 
-// @Roles('admin') , RolesGuard
 
-@UseGuards(AuthGuard('jwt'))
+
+@UseGuards(AuthGuard('jwt'), RolesGuard)
+@Roles('admin')
 @Controller('users')
 export class UserController {
   constructor(private userService: UserService) {}
